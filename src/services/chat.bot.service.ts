b18263e19c0ae4b111c1f8530dc712bf12d0ec6f
Jahidul Islam <jahid.info97@gmail.com>
2025-02-05 19:42:29 +0600

    feat(chat-backend): system message added

--- src/services/chat.bot.service.ts
+++ src/services/chat.bot.service.ts
@@ -3,15 +3,16 @@ import { Message } from "@main/models/chat.model";
 
 
 const getMessageFromBot = async (message: Message[], images: string[] | null, chatType: String): Promise<string> => {
-
     const response = await ollama.chat({
         model: 'llama3.1',
-        messages: message.map((msg) => ({
+        messages: [{
+            'role': 'system',
+            'content': 'You are an AI assistant named Jarvis',
+        },], ...message.map((msg) => ({
             role: msg.who,
             content: msg.message
         })),
     })
-
     return response.message.content;
 
 };
